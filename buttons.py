206cb1b09977915ba9d0b55a052f7388121d52f9
Guilherme <gui@GUI-BAZOOKA>
2021-09-06 18:42:31 -0300

    Shuffle icon

--- buttons.py
+++ buttons.py
@@ -2,28 +2,17 @@
 
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.image import Image  
 from kivy.properties import ListProperty, NumericProperty, BooleanProperty, StringProperty, ObjectProperty
 from kivy.uix.behaviors import ButtonBehavior, CompoundSelectionBehavior
 from behaviors import MouseOverBehavior
 
 
 class ButtonLabel(ButtonBehavior, MouseOverBehavior, Label):
-    def __init__(self, **kwargs):
-        super(ButtonLabel, self).__init__(**kwargs)
-        
-        self.normal_color = (1, 1, 1, 1)
-        self.toggle_color = (0, 1, 1, 1)
+    pass
 
-    def on_press(self):
-        return
-        self.color = self.toggle_color
-        Clock.schedule_once(self.unset_color, 0.1)
-        pass
-
-    def unset_color(self, *args):
-        return
-        self.color = self.normal_color
-        pass
+class ButtonImage(ButtonBehavior, MouseOverBehavior, Image):
+    pass
 
 class ButtonLabelAddSongToPlaylist(ButtonLabel):
     def unset_color(self, *args):
@@ -43,3 +32,10 @@ class ButtonLabelToggle(ButtonBehavior, MouseOverBehavior, Label):
         self.toggle = not self.toggle
         print(self.toggle)
 
+
+class ButtonImageToggle(ButtonBehavior, MouseOverBehavior, Image):
+    toggle = BooleanProperty(False)
+
+    def on_release(self, *args):
+        self.toggle = not self.toggle
+        print(self.toggle)
